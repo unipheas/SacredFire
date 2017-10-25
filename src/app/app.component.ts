9c726db9ac261cdf6b1795e78ad955d425e1cd56
Brian Phillips <brian.phillips@qq.com>
2017-10-25 20:58:57 +0800

    Login: ✔ setup app.component to handle login process. @done (2017-10-25 20:39:29) Menu: ✔ HomePage @done (2017-10-25 20:50:12) ✔ Logout @done (2017-10-25 20:50:21) Updated Tasks.todo file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { CallbackPage } from './../pages/callback/callback';
-// import { LoginPage } from './../pages/login/login';
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+
+
 import Auth0Cordova from '@auth0/cordova';
+import { AuthProvider } from './../providers/auth/auth';
 
 @Component({
   templateUrl: 'app.html'
@@ -19,15 +19,17 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public auth: AuthProvider
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage },
-      { title: 'Callback', component: CallbackPage },
-
+      { title: 'Home', component: HomePage }
     ];
 
   }
@@ -43,6 +45,11 @@ export class MyApp {
       (<any>window).handleOpenURL = (url) => {
         Auth0Cordova.onRedirectUri(url);
       };
+
+      // setup login here.
+      if (!this.auth.isAuthenticated()) {
+        this.auth.login();
+      }
     });
   }
 
@@ -50,5 +57,10 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  logout() {
+    this.auth.logout();
+    this.platform.exitApp();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
-import { CallbackPage } from './../pages/callback/callback';
-import { LoginPage } from './../pages/login/login';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -16,10 +12,7 @@ import { AuthProvider } from '../providers/auth/auth';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage,
-    LoginPage,
-    CallbackPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -28,10 +21,7 @@ import { AuthProvider } from '../providers/auth/auth';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage,
-    LoginPage,
-    CallbackPage
+    HomePage
   ],
   providers: [
     StatusBar,
